1c3ea382860aae9bb1decefc8392625ae17032b4
kenny-kogi <mwangiken497@gmail.com>
2021-08-11 20:26:02 +0300

    events page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_144131) do
+ActiveRecord::Schema.define(version: 2021_08_11_152012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_144131) do
     t.string "name"
     t.text "description"
     t.string "even_location"
-    t.string "type"
+    t.string "type_event"
     t.string "category"
     t.date "start_date"
     t.date "end_date"
