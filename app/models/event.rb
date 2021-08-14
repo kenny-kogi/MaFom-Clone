1f99a5b68076bc7be3df5a4c661b806083e4553f
kenny-kogi <mwangiken497@gmail.com>
2021-08-14 19:18:46 +0300

    events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     has_one_attached :poster, :dependent => :destroy
+    CATEGORY = ['Community and Culture', ' Music', 'Food and Drinks', 'Religious Events', 'Business and Profession']
+    TYPE_EVENT = ['Conference', 'Party/Social Gathering', 'Dinner/Gala', 'Game/Competition', 'Concert']
 end
